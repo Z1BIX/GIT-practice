7e2606b2031574328a2717649b57e7e40948ec1e
asoloveii <arsenisoloveii@gmail.com>
2024-10-08 09:44:01 +0200

    Added the beginning of my greatest code Implemented add method Implemented subtract method in Subtractor Implemented subtract method in Subtractor deleted subtractor updated subtract

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,6 @@ public class Main {
         System.out.println(adder.add(6, 6));
 
         Subtractor subtractor = new Subtractor();
-        System.out.println(subtractor.subtract(6, 3));
+        System.out.println(subtractor.subtract(3, 3));
     }
 }
