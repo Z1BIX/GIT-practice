4cbd699652d59665aeef9113c27ff8e30b994839
Z1BIX <maxgolovatenko@gmail.com>
2024-10-08 09:09:24 +0200

    Task distribution

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 // TODO: we need to add the missing classes!
 
+//Ok, I will add 'Adder' and s31725 will add 'Subtractor'.
 public class Main {
     public static void main(String[] args) {
         Adder adder = new Adder();
